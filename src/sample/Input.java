8ef27bef2fc9461c82c84ee2803c572e096c970f
Josh <joshvelt@outlook.com>
2019-01-25 03:09:33 -0800

    Multithreading & Initial conversion code implemented

--- src/sample/Input.java
+++ src/sample/Input.java
@@ -2,12 +2,14 @@ package sample;
 
 public class Input {
 
+    int[][] keyArray = new int[6][8];
+
     public void keyDect() {
 
         /*Instantiates array keyArray which acts as a reference for keyboard to piano handoff. Rows are "asdfghj" on the keyboard.
          Columns are 1-9. Combining will lookup in array and give A1 B7 etc.
          */
-        int[][] keyArray = new int[6][8];
+
         for (int i = 0; i < 63; i++) {
             int x = 0;
             int y = 0;
@@ -21,7 +23,31 @@ public class Input {
                 y++;
 
 
+
             }
         }
+
     }
+    public int toNote(int n, int o){
+
+        if (n <= 6 && o <= 8){
+            System.out.println(keyArray[n][o]);
+            return keyArray[n][o];
+
+        } else {
+            System.out.println("Invalid Note entry");
+            return (0);
+        }
+
+    }
+
+    //public int fromString(String key){
+
+
+
+    //}
+
+
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -14,12 +14,46 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        for (int i=0; i<4; i++)
+        {
+            Thread object = new Thread(new Multithread());
+            object.start();
+        }
         Input keyInit = new Input();
         keyInit.keyDect();
+        
+
+
     }
+    class Multithread implements Runnable
+    {
+        public void run()
+        {
+            try
+            {
+                // Displaying the thread that is running
+                System.out.println ("Thread " +
+                        Thread.currentThread().getId() +
+                        " is running");
+
+            }
+            catch (Exception e)
+            {
+                // Throwing an exception
+                System.out.println ("Exception is caught");
+            }
+        }
+
+    }
+
 
 
     public static void main(String[] args) {
         launch(args);
+
+
     }
+
+
+
 }
